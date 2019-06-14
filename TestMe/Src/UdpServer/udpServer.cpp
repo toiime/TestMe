b39e585ad14a9f238813e7ea4f89ebf12a11fec6
DESKTOP-A0VLH7H\toiim <toiime@sina.com>
2019-06-15 01:37:49 +0800

    UDP发送端

--- TestMe/Src/UdpServer/udpServer.cpp
+++ TestMe/Src/UdpServer/udpServer.cpp
@@ -1,12 +1,53 @@
-#include "udpServer.h"
+﻿#include "udpServer.h"
+
+#include <QMessageBox>
+#include <QKeyEvent>
+
 
 UdpServer::UdpServer(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+
+	_qUdpSocket = new QUdpSocket;
+	_qHostAddress = "127.0.0.1";
+	_port = 5400;
+
+	// 绑定信号槽
+	connect(ui.pushButtonSend, &QPushButton::clicked, this, &UdpServer::SlotSendText);
+
+	// 设置鼠标焦点
+	ui.lineEditSendText->setFocus();
 }
 
 UdpServer::~UdpServer()
 {
+	if (_qUdpSocket) {
+		delete _qUdpSocket;
+		_qUdpSocket = nullptr;
+	}
+}
 
+void UdpServer::SlotSendText() {
+	QString sendText = ui.lineEditSendText->text();
+	ui.lineEditSendText->clear();
+
+	if (sendText.isEmpty()) {
+		return;
+	}
+
+	QString textEditText = ui.textEditText->toPlainText();
+	textEditText += sendText;
+	textEditText += "\n";
+	ui.textEditText->setPlainText(textEditText);
+
+	QByteArray msg = sendText.toUtf8();
+	_qUdpSocket->writeDatagram(msg, _qHostAddress, _port);
+}
+
+void UdpServer::keyPressEvent(QKeyEvent *event) {
+	if (event->key() == Qt::Key_Return // 大键盘回车键
+		|| event->key() == Qt::Key_Enter) { // 小键盘 Enter 键
+		SlotSendText();
+	}
 }
--- TestMe/Src/UdpServer/udpServer.h
+++ TestMe/Src/UdpServer/udpServer.h
@@ -1,8 +1,10 @@
-#ifndef UDPSERVER_H
+﻿#ifndef UDPSERVER_H
 #define UDPSERVER_H
 
 #include <QtWidgets/QWidget>
 #include "ui_udpServer.h"
+
+#include <QUdpSocket>
 
 class UdpServer : public QWidget
 {
@@ -12,8 +14,18 @@ public:
 	UdpServer(QWidget *parent = 0);
 	~UdpServer();
 
+protected:
+	void keyPressEvent(QKeyEvent *event);
+
+private:
+	void SlotSendText();
+
 private:
 	Ui::UdpServerClass ui;
+
+	QUdpSocket* _qUdpSocket;
+	QHostAddress _qHostAddress;  // IP
+	quint16 _port;               // 端口
 };
 
 #endif // UDPSERVER_H
